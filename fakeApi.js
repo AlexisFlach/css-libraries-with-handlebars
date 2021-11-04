dd61669df052f1b5ec8c7add11e482726d9463f0
alexisflach <alexis.flach87@gmail.com>
2021-11-04 22:51:25 +0100

    done setup

---
+++ fakeApi.js
@@ -0,0 +1,28 @@
+const fakeApi = () => {
+    return [
+        {
+            id: 1,
+            title: "React",
+            excerpt: "Cool React Project"
+        },
+        {
+            id: 2,
+            title: "SQL",
+            excerpt: "Cool SQL Project"
+        },
+        {
+            id: 3,
+            title: "Django",
+            excerpt: "Cool Django Project"
+        },
+        {
+            id: 4,
+            title: "Next.js",
+            excerpt: "Cool Next.js Project"
+        },
+        
+        
+    ]
+}
+
+module.exports = fakeApi;
--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express();
-
 const PORT = 3000;
 
+const fakeApi = require('./fakeApi');
+
+const handlebars = require('express-handlebars');
+
+const hasProjects = true;
+
+app.set('view engine', 'hbs')
+
+app.engine('hbs', handlebars({
+    layoutsDir: `${__dirname}/views/layouts`,
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialsDir: `${__dirname}/views/partials`
+}))
+
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
 
 app.get('/', (req, res) => {
-    res.send('Hello My World')
+    res.render('main', { layout: 'index' })
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects', { layout: 'index', projects: fakeApi(), hasProjects: hasProjects })
 })
 
 app.listen(PORT, () => {
