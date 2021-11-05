ee10ee1f88170e5f90096fbf3daa6f0e165d6fcd
alexisflach <alexis.flach87@gmail.com>
2021-11-05 08:22:18 +0100

    init bootstrap

---
+++ setup/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+const fakeApi = require('./fakeApi');
+
+const handlebars = require('express-handlebars');
+
+const hasProjects = true;
+
+app.set('view engine', 'hbs')
+
+app.engine('hbs', handlebars({
+    layoutsDir: `${__dirname}/views/layouts`,
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialsDir: `${__dirname}/views/partials`
+}))
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('main', { layout: 'index' })
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects', { layout: 'index', projects: fakeApi(), hasProjects: hasProjects })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listens on port: ${PORT}`);
+})
